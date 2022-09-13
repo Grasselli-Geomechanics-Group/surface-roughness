822e0ac3086423c17f8949d57e335f5e15a5b796
e-mags <33796821+e-mags@users.noreply.github.com>
2022-09-13 15:04:17 -0400

    Take advantage of inheritance in pybind code

--- surface_roughness/_roughness_cppimpl.cpp
+++ surface_roughness/_roughness_cppimpl.cpp
@@ -38,10 +38,6 @@ PYBIND11_MODULE(_roughness_cppimpl,m) {
         .def("__setitem__",&DirectionalSetting::set)
         .def("__getitem__",&DirectionalSetting::get);
 
-    m.def("_cppDirectionalRoughness_Settings_impl",[](){return DirectionalRoughness::Setting();});
-    m.def("_cppTINBasedRoughness_Settings_impl",[](){return TINBasedRoughness::Setting();});
-    m.def("_cppMeanDipRoughness_Settings_impl",[](){return MeanDipRoughness::Setting();});
-
     py::class_<Directional,PyDirectional>(m,"_cppDirectional_impl")
         .def(py::init<Eigen::MatrixX3d,Eigen::MatrixX3i>())
         .def(py::init<Eigen::MatrixX3d,Eigen::MatrixX3i,Eigen::ArrayXi>())
@@ -57,80 +53,29 @@ PYBIND11_MODULE(_roughness_cppimpl,m) {
         .def_property_readonly("shape_size",&Directional::get_size)
         .def_property_readonly("total_area",&Directional::get_area);
 
+    m.def("_cppDirectionalRoughness_Settings_impl",[](){return DirectionalRoughness::Setting();});
+    m.def("_cppTINBasedRoughness_Settings_impl",[](){return TINBasedRoughness::Setting();});
+    m.def("_cppMeanDipRoughness_Settings_impl",[](){return MeanDipRoughness::Setting();});
+
+
     py::class_<DirectionalRoughness,Directional>(m,"_cppDirectionalRoughness_impl")
         .def(py::init<Eigen::MatrixX3d,Eigen::MatrixX3i>())
-        .def(py::init<Eigen::MatrixX3d,Eigen::MatrixX3i,Eigen::ArrayXi>())
-        .def("evaluate",&DirectionalRoughness::evaluate)
-        .def("__getitem__",&DirectionalRoughness::operator[])
-        .def("points",&DirectionalRoughness::get_points)
-        .def("normals",&DirectionalRoughness::get_normals)
-        .def("result_keys",&DirectionalRoughness::result_keys)
-        .def_property_readonly("final_orientation",&DirectionalRoughness::get_final_orientation)
-        .def_property_readonly("min_bounds",&DirectionalRoughness::get_min_bounds)
-        .def_property_readonly("max_bounds",&DirectionalRoughness::get_max_bounds)
-        .def_property_readonly("centroid",&DirectionalRoughness::get_centroid)
-        .def_property_readonly("shape_size",&DirectionalRoughness::get_size)
-        .def_property_readonly("total_area",&DirectionalRoughness::get_area);
-
-
+        .def(py::init<Eigen::MatrixX3d,Eigen::MatrixX3i,Eigen::ArrayXi>());
+;
     py::class_<TINBasedRoughness,Directional>(m,"_cppTINBasedRoughness_impl")
         .def(py::init<Eigen::MatrixX3d,Eigen::MatrixX3i>())
-        .def(py::init<Eigen::MatrixX3d,Eigen::MatrixX3i,Eigen::ArrayXi>())
-        .def("evaluate",&TINBasedRoughness::evaluate)
-        .def("__getitem__",&TINBasedRoughness::operator[])
-        .def("points",&TINBasedRoughness::get_points)
-        .def("normals",&TINBasedRoughness::get_normals)
-        .def("result_keys",&TINBasedRoughness::result_keys)
-        .def_property_readonly("final_orientation",&TINBasedRoughness::get_final_orientation)
-        .def_property_readonly("min_bounds",&TINBasedRoughness::get_min_bounds)
-        .def_property_readonly("max_bounds",&TINBasedRoughness::get_max_bounds)
-        .def_property_readonly("centroid",&TINBasedRoughness::get_centroid)
-        .def_property_readonly("shape_size",&TINBasedRoughness::get_size)
-        .def_property_readonly("total_area",&TINBasedRoughness::get_area);
+        .def(py::init<Eigen::MatrixX3d,Eigen::MatrixX3i,Eigen::ArrayXi>());
 
     py::class_<TINBasedRoughness_bestfit,Directional>(m,"_cppTINBasedRoughness_bestfit_impl")
         .def(py::init<Eigen::MatrixX3d,Eigen::MatrixX3i>())
-        .def(py::init<Eigen::MatrixX3d,Eigen::MatrixX3i,Eigen::ArrayXi>())
-        .def("evaluate",&TINBasedRoughness_bestfit::evaluate)
-        .def("__getitem__",&TINBasedRoughness_bestfit::operator[])
-        .def("points",&TINBasedRoughness_bestfit::get_points)
-        .def("normals",&TINBasedRoughness_bestfit::get_normals)
-        .def("result_keys",&TINBasedRoughness_bestfit::result_keys)
-        .def_property_readonly("final_orientation",&TINBasedRoughness_bestfit::get_final_orientation)
-        .def_property_readonly("min_bounds",&TINBasedRoughness_bestfit::get_min_bounds)
-        .def_property_readonly("max_bounds",&TINBasedRoughness_bestfit::get_max_bounds)
-        .def_property_readonly("centroid",&TINBasedRoughness_bestfit::get_centroid)
-        .def_property_readonly("shape_size",&TINBasedRoughness_bestfit::get_size)
-        .def_property_readonly("total_area",&TINBasedRoughness_bestfit::get_area);
+        .def(py::init<Eigen::MatrixX3d,Eigen::MatrixX3i,Eigen::ArrayXi>());
 
     py::class_<TINBasedRoughness_againstshear,Directional>(m,"_cppTINBasedRoughness_againstshear_impl")
         .def(py::init<Eigen::MatrixX3d,Eigen::MatrixX3i>())
-        .def(py::init<Eigen::MatrixX3d,Eigen::MatrixX3i,Eigen::ArrayXi>())
-        .def("evaluate",&TINBasedRoughness_againstshear::evaluate)
-        .def("__getitem__",&TINBasedRoughness_againstshear::operator[])
-        .def("points",&TINBasedRoughness_againstshear::get_points)
-        .def("normals",&TINBasedRoughness_againstshear::get_normals)
-        .def("result_keys",&TINBasedRoughness_againstshear::result_keys)
-        .def_property_readonly("final_orientation",&TINBasedRoughness_againstshear::get_final_orientation)
-        .def_property_readonly("min_bounds",&TINBasedRoughness_againstshear::get_min_bounds)
-        .def_property_readonly("max_bounds",&TINBasedRoughness_againstshear::get_max_bounds)
-        .def_property_readonly("centroid",&TINBasedRoughness_againstshear::get_centroid)
-        .def_property_readonly("shape_size",&TINBasedRoughness_againstshear::get_size)
-        .def_property_readonly("total_area",&TINBasedRoughness_againstshear::get_area);
+        .def(py::init<Eigen::MatrixX3d,Eigen::MatrixX3i,Eigen::ArrayXi>());
 
     py::class_<MeanDipRoughness,Directional>(m,"_cppMeanDipRoughness_impl")
         .def(py::init<Eigen::MatrixX3d,Eigen::MatrixX3i>())
-        .def(py::init<Eigen::MatrixX3d,Eigen::MatrixX3i,Eigen::ArrayXi>())
-        .def("evaluate",&MeanDipRoughness::evaluate)
-        .def("__getitem__",&MeanDipRoughness::operator[])
-        .def("points",&MeanDipRoughness::get_points)
-        .def("normals",&MeanDipRoughness::get_normals)
-        .def("result_keys",&MeanDipRoughness::result_keys)
-        .def_property_readonly("final_orientation",&MeanDipRoughness::get_final_orientation)
-        .def_property_readonly("min_bounds",&MeanDipRoughness::get_min_bounds)
-        .def_property_readonly("max_bounds",&MeanDipRoughness::get_max_bounds)
-        .def_property_readonly("centroid",&MeanDipRoughness::get_centroid)
-        .def_property_readonly("shape_size",&MeanDipRoughness::get_size)
-        .def_property_readonly("total_area",&MeanDipRoughness::get_area);
+        .def(py::init<Eigen::MatrixX3d,Eigen::MatrixX3i,Eigen::ArrayXi>());
 
 }
